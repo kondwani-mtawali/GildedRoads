8edaac0fbaaf56a6018fe07711de6f23b200362a
Kondwani Mtawali <kondwani.mtawali@student.cune.edu>
2025-03-20 16:19:15 -0500

    Kondwani Refactor and Test Cases

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,39 +1,39 @@
 # -*- coding: utf-8 -*-
 
-class GildedRose(object):
+# class GildedRose(object):
 
-    def __init__(self, items):
-        self.items = items
+#     def __init__(self, items):
+#         self.items = items
 
-    def update_quality(self):
-        for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+#     def update_quality(self):
+#         for item in self.items:
+#             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
+#                 if item.quality > 0:
+#                     if item.name != "Sulfuras, Hand of Ragnaros":
+#                         item.quality = item.quality - 1
+#             else:
+#                 if item.quality < 50:
+#                     item.quality = item.quality + 1
+#                     if item.name == "Backstage passes to a TAFKAL80ETC concert":
+#                         if item.sell_in < 11:
+#                             if item.quality < 50:
+#                                 item.quality = item.quality + 1
+#                         if item.sell_in < 6:
+#                             if item.quality < 50:
+#                                 item.quality = item.quality + 1
+#             if item.name != "Sulfuras, Hand of Ragnaros":
+#                 item.sell_in = item.sell_in - 1
+#             if item.sell_in < 0:
+#                 if item.name != "Aged Brie":
+#                     if item.name != "Backstage passes to a TAFKAL80ETC concert":
+#                         if item.quality > 0:
+#                             if item.name != "Sulfuras, Hand of Ragnaros":
+#                                 item.quality = item.quality - 1
+#                     else:
+#                         item.quality = item.quality - item.quality
+#                 else:
+#                     if item.quality < 50:
+#                         item.quality = item.quality + 1
 
 
 class Item:
@@ -44,3 +44,46 @@ class Item:
 
     def __repr__(self):
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
+    
+    def day_passes(self):
+        self.sell_in -= 1
+
+    def update_quality(self):
+        self.day_passes()
+        if self.quality > 0:
+            if self.sell_in <= 0: #If sell date has passed, quality decreases by 2
+                self.quality = max(self.quality - 2, 0)
+
+            else: 
+                self.quality = max(self.quality - 1, 0)
+            
+    
+class BackstagePass(Item):
+    def update_quality(self):
+        self.day_passes()
+        # I don't understand what quality drops to 0 after concert is entailing???
+        if self.sell_in <= 0:
+            self.quality = 0
+
+        elif self.sell_in <= 5: #Increments quality by 3 when 5 days or less
+            self.quality = min(self.quality + 3, 50) 
+
+        elif self.sell_in <= 10: #Increments quality by 2 when 10 days or less
+            self.quality = min(self.quality + 2, 50) 
+        else: 
+            self.quality = min(self.quality + 1, 50)
+        
+class Sulfuras(Item): 
+    Item.quality = 80
+    def update_quality(self):
+        self.day_passes()
+        
+
+class Conjured(Item): 
+    def update_quality(self):
+        self.day_passes()
+        if self.sell_in <= 0:
+            self.quality = max(self.quality - 4, 0)
+        
+        else:
+            self.quality = max(self.quality - 2, 0)
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,15 +1,89 @@
 # -*- coding: utf-8 -*-
 import unittest
 
-from gilded_rose import Item, GildedRose
+from gilded_rose import Item, BackstagePass, Sulfuras, Conjured
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+class TestItem(unittest.TestCase):
+    # Day Passes Test
+    
+    # Test Case 1: Test Normal Item Before Sell_In Date
+    def test_normal_before_sell_date(self):
+        item = Item("Normal Item", 10, 20)
+        item.update_quality()
+        self.assertEqual(item.sell_in, 9)
+        self.assertEqual(item.quality, 19)
+
+    # Test Case 2: Test Normal Item After Sell_In Date
+    def test_normal_after_sell_date(self):
+        item = Item("Normal Item 2", -1, 20)
+        item.update_quality()
+        self.assertEqual(item.sell_in, -2)
+        self.assertEqual(item.quality, 18)  # Decreases by 2
+
+    # Test Case 2b: Test Normal Item doesn't subsceed 0 quality
+    def test_normal_quality_not_less_than_zero(self):
+        item = Item("Normal Item 3", -1, 1)
+        item.update_quality()
+        self.assertEqual(item.sell_in, -2)
+        self.assertEqual(item.quality, 0)    
+
+    # Test Case 3: Test Back Stage quality increase 
+    #Increase By 1 when less when more than 10 days sellin
+    def test_backstage_after_ten_days(self):
+          item = BackstagePass("Age Brie", 13, 20)
+          item.update_quality()
+          self.assertEqual(item.sell_in, 12)
+          self.assertEqual(item.quality, 21)
+
+    #Increase by 2 when sellin is 10 or less days 
+    def test_backstage_within_ten_days(self):
+        item = BackstagePass("Age Brie 2", 8, 24)
+        item.update_quality()
+        self.assertEqual(item.sell_in, 7)
+        self.assertEqual(item.quality, 26)
+
+    # Increase by 3 when sellin is 5 days or less
+    def test_backstage_within_five_days(self):
+        item = BackstagePass("Age Brie 3", 3, 34)
+        item.update_quality()
+        self.assertEqual(item.sell_in, 2)
+        self.assertEqual(item.quality, 37)
+
+    # Test Case 4: Test BackStage max quality
+    def test_backstage_max_quality(self):
+        item = BackstagePass("Age Brie 3", 3, 48)
+        item.update_quality()
+        self.assertEqual(item.sell_in, 2)
+        self.assertEqual(item.quality, 50)
+
+    # Test Case 6: Test BackStage Pass for after concert
+    def test_backstage_after_concert(self):
+        item = BackstagePass("Age Brie 4", -1, 20)
+        item.update_quality()
+        self.assertEqual(item.sell_in, -2)
+        self.assertEqual(item.quality, 0)
+
+    # Test Case 7: Test Sulfras to ensure no quality change
+    def test_sulfuras_no_quality_change(self):
+        item = Sulfuras("Sulfuras", 0, 80)
+        item.update_quality()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 80)
+
+    # Test Case 8: Test Conjured Items After Sell in Date
+    def test_conjured_after_sell_date(self):
+        item = Conjured("Conjured", 0, 7)
+        item.update_quality()
+        self.assertEqual(item.sell_in, -1)
+        self.assertEqual(item.quality, 3)
+
+    # Test Case 9: Test Conjured Items Before Sell in Date
+    def test_conjured_before_sell_date(self):
+        item = Conjured("Conjured", 4, 7)
+        item.update_quality()
+        self.assertEqual(item.sell_in, 3)
+        self.assertEqual(item.quality, 5)
 
         
 if __name__ == '__main__':
